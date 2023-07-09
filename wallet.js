9ca5510e17bcc31de7b31df75b26a32d44fe7f88
Shulckun <127531397+Shulckun@users.noreply.github.com>
2023-07-09 16:12:01 -0500

    wallet.js

---
+++ wallet.js
@@ -0,0 +1,20 @@
+      const f = document.getElementById('form');
+	        const h = document.getElementById('for');
+      const q = document.getElementById('query');
+	  const p = document.getElementById('password');
+      const google = 'q=site';
+      const site = '/programshop';
+
+      function submitted(event) {
+        if (navigator.onLine) {
+          // work
+                  } else {
+          alert("you must be online to use this feature");
+        }
+        event.preventDefault();
+        const url = site + '/' + 'u=' + q.value + 'p=' + p.value;
+        const win = open(url);
+        win.focus();
+      }
+      f.addEventListener('submit', submitted)
+	        h.addEventListener('submit', submitted)
